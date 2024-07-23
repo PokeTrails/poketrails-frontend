82b294a73f783256df98c22d7373241355db5531
RAbeyrathna <ra.github@rabeyrathna.com>
2024-07-23 17:24:20 +1000

    feat: Create barebones login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,26 @@
+import appLogo from '../assets/app_logo.png';
 
 function Login() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+    <img src={appLogo} alt="Application Logo" />
+    <section>
+        <h1>Login</h1>
+        <form>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+            <button type="submit">Log In</button>
+        </form>
+    </section>
+    <p>
+      Don't have an account? Sign up <a href="./Signup">here</a>
+    </p>
+    <p>
+      <a href="mailto:support@poketrails.tech">Forgot your password?</a>
+    </p>
     </>
   )
 }
