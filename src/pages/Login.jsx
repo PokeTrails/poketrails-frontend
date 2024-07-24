76c3c0ab333092df32355774e6e5a30cb484f98d
RAbeyrathna <ra.github@rabeyrathna.com>
2024-07-24 20:06:35 +1000

    feat: Add parent box for sign up and log in page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,12 @@
+import Box from '@mui/material/Box';
+
 import AppLogo from '../components/AppLogo';
 import LoginForm from '../components/LoginForm';
 
 function Login() {
   return (
     <>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         <AppLogo />
         <LoginForm />
         <p>
@@ -12,6 +15,7 @@ function Login() {
         <p>
             <a href="mailto:support@poketrails.tech">Forgot your password?</a>
         </p>
+        </Box>
     </>
   )
 }
