90b42f2d7405574bb3b6f164601e4f9842dae276
RAbeyrathna <ra.github@rabeyrathna.com>
2024-07-24 18:29:25 +1000

    feat: Create basic navbar component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from '../components/Navbar';
 import Login from './Login';
 import Signup from './Signup';
 
 function App() {
   return (
     <BrowserRouter>
+      <CssBaseline />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
