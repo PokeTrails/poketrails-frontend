04c88f395ddbb7ce06b0d36c396300f0c4e083e7
RAbeyrathna <ra.github@rabeyrathna.com>
2024-07-27 09:21:08 +1000

    feat: Add connection to backend for login requests

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,8 @@
 import * as React from 'react';
+import { useState } from 'react';
+
+import axios from 'axios';
+
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import OutlinedInput from '@mui/material/OutlinedInput';
@@ -10,8 +14,13 @@ import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { Button } from '@mui/material';
 
+const apiURL = import.meta.env.VITE_API_SERVER_URL;
+
 export default function LoginForm() {
   const [showPassword, setShowPassword] = React.useState(false);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -19,11 +28,34 @@ export default function LoginForm() {
     event.preventDefault();
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!password) {
+      alert('Please enter a password');
+      return;
+    }
+    try {
+      const response = await axios.post(`${apiURL}/login`, {
+        username,
+        password,
+      });
+      console.log(response.data);
+      alert('Login successful');
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  };
+
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: "20px" }}>
+    <Box component="form"
+    onSubmit={handleSubmit}
+    sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: "20px" }}>
         <TextField
           label="Username"
           id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
           sx={{ m: 1, width: '25ch' }}
         />
         <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
@@ -31,6 +63,8 @@ export default function LoginForm() {
           <OutlinedInput
             id="password"
             type={showPassword ? 'text' : 'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -46,7 +80,7 @@ export default function LoginForm() {
             label="Password"
           />
         </FormControl>
-        <Button variant="contained" size="medium" sx={{width: "60%", marginTop: "10px"}}>Log In</Button>
+        <Button type="submit" variant="contained" size="medium" sx={{width: "60%", marginTop: "10px"}}>Log In</Button>
     </Box>
   );
 }
