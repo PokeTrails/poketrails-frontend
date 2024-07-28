168b861171d2b76f7e2dbbdf0626ff1a06b21720
RAbeyrathna <ra.github@rabeyrathna.com>
2024-07-28 17:35:26 +1000

    fix: Update navbar imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,7 @@
 import * as React from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
+import { AppBar, Box, Toolbar, Typography, IconButton, Menu, MenuIcon, MenuItem, Container, Avatar, Button, Tooltip } from '@mui/material';
+
 import appLogo from '../assets/app_logo.png';
 
 const pages = ['Home', 'Party', 'Trails', 'Pokédex', 'Store'];
