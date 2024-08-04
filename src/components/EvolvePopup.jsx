15772c83a0b253b78dd4b3ef7bff437166f19d23
RAbeyrathna <ra.github@rabeyrathna.com>
2024-08-04 19:59:52 +1000

    feat: Evolve button updates pokemon data to next stage

--- src/components/EvolvePopup.jsx
+++ src/components/EvolvePopup.jsx
@@ -1,9 +1,20 @@
 import { Box, Typography, Button } from '@mui/material';
 import PropTypes from 'prop-types';
-
-import { capitaliseName } from '../utils';
+import { useEffect, useState } from 'react';
 
 const EvolvePopup = ({ data, onClose, onReload }) => {
+  const [showNewSprite, setShowNewSprite] = useState(false);
+
+  useEffect(() => {
+    if (data) {
+      const timer = setTimeout(() => {
+        setShowNewSprite(true);
+      }, 1000); // Delay before showing the new sprite
+
+      return () => clearTimeout(timer);
+    }
+  }, [data]);
+
   const handleClose = () => {
     onClose();
     if (onReload) onReload();
@@ -27,15 +38,47 @@ const EvolvePopup = ({ data, onClose, onReload }) => {
         overflow: 'hidden',
       }}
     >
-
       <Typography variant="h6" gutterBottom>
-        `${capitaliseName(data.nickname)}` is evolving!
+        `{data.nickname}` is evolving!
       </Typography>
       {data && (
         <Box>
-          <img src={data.sprite} alt={data.species} style={{ maxWidth: '100%' }} />
+          <Box
+            sx={{
+              position: 'relative',
+              height: '200px', // Adjust based on your sprite size
+              width: 'auto',
+              overflow: 'hidden',
+              margin: 'auto',
+            }}
+          >
+            <img
+              src={data.oldSprite}
+              alt="Old Sprite"
+              style={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                objectFit: 'contain',
+                transition: 'opacity 1s',
+                opacity: showNewSprite ? 0 : 1,
+              }}
+            />
+            <img
+              src={data.sprite}
+              alt="New Sprite"
+              style={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                objectFit: 'contain',
+                transition: 'opacity 1s',
+                opacity: showNewSprite ? 1 : 0,
+              }}
+            />
+          </Box>
           <Typography variant="h6" gutterBottom>
-            {capitaliseName(data.species)}
+            {data.species}
           </Typography>
         </Box>
       )}
@@ -47,7 +90,12 @@ const EvolvePopup = ({ data, onClose, onReload }) => {
 };
 
 EvolvePopup.propTypes = {
-  data: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    nickname: PropTypes.string.isRequired,
+    oldSprite: PropTypes.string.isRequired,
+    sprite: PropTypes.string.isRequired,
+    species: PropTypes.string.isRequired,
+  }).isRequired,
   onClose: PropTypes.func.isRequired,
   onReload: PropTypes.func,
 };
