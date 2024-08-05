01c5af93f2102e19f78efc8d729b5100dfc8bd38
RAbeyrathna <ra.github@rabeyrathna.com>
2024-08-05 13:51:03 +1000

    feat: Create TrailBoxComponent and add party component to it

---
+++ src/components/TrailComponentBox.jsx
@@ -0,0 +1,52 @@
+import { Box, Typography } from '@mui/material';
+import PropTypes from 'prop-types';
+
+import PokemonParty from './UserParty';
+
+export default function TrailComponentBox({ componentDetails }) {
+    const jwt = localStorage.getItem('jwt');
+    const apiURL = `${import.meta.env.VITE_API_SERVER_URL}`;
+    
+    const handlePokemonSelect = (pokemon) => {
+      setSelectedPokemon(pokemon);
+    };
+
+  return (
+    <>
+        {/* Overall Box Component holding everything */}
+        <Box
+        sx={{
+            mt: 2,
+            backgroundColor: 'rgba(164, 218, 195, 0.5)',
+            width: { xs: '100%', md: '100%' },
+            maxWidth: '1200px',
+            borderRadius: 2,
+        }}
+        >
+        {/* Heading Box for Component name */}
+        <Box
+            sx={{
+            backgroundColor: 'rgba(122, 220, 185, 0.6)',
+            pt: 1,
+            pb: 0.5,
+            mb: 1,
+            borderTopLeftRadius: 2,
+            borderTopRightRadius: 2,
+            }}
+        >
+            <Typography variant="h4" fontSize={{ xs: '20px', md: '25px' }} gutterBottom textAlign="center">
+            {componentDetails.heading}
+            </Typography>
+        </Box>
+        {/* Main Content goes here */}
+            Test
+        </Box>
+        <PokemonParty apiURL={apiURL} jwt={jwt} onPokemonSelect={handlePokemonSelect} />
+    </>
+  );
+}
+
+TrailComponentBox.propTypes = {
+  componentDetails: PropTypes.object,
+  children: PropTypes.node.isRequired,
+};
--- src/pages/WildTrail.jsx
+++ src/pages/WildTrail.jsx
@@ -1,6 +1,28 @@
+import { Box } from "@mui/material";
+
+import Background from "../components/Background";
+import backgroundImg from '../assets/trail_images/wild_trail.png';
+import TrailComponentBox from "../components/TrailComponentBox";
+
+const componentDetails = {
+  heading: "Wild Trail"
+};
 
 export default function WildTrail() {
     return (
-      <h1>Wild Trail Page</h1>
+      // Import background component and pass through page specifc image/colours
+      <Background backgroundImg={backgroundImg} backgroundColour="#C9EECF">
+        <Box sx={{
+          mt: 3,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '100%',
+          height: '100vh',
+        }}>
+          {/* Main Component to render */}
+          <TrailComponentBox componentDetails={componentDetails}/>
+        </Box>
+      </Background>
     );
   }
