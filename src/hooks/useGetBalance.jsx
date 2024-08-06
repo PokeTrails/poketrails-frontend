f757a238ba703765e25e8e948b0888382572069c
RAbeyrathna <ra.github@rabeyrathna.com>
2024-08-06 23:24:05 +1000

    feat: Create hook to get user balance and egg vouchers

---
+++ src/hooks/useGetBalance.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const jwt = localStorage.getItem('jwt');
+const apiURL = `${import.meta.env.VITE_API_SERVER_URL}`;
+
+function useGetBalance() {
+  const [balance, setBalance] = useState(null);
+  const [vouchers, setVouchers] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!jwt) return;
+
+    const getBalance = async () => {
+      try {
+        const response = await axios.get(`${apiURL}/user/balance`, {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        });
+        setBalance(response.data.balance);
+        setVouchers(response.data.vouchers);
+      } catch (err) {
+        console.error('Error fetching donation reward:', err);
+        setError('Failed to fetch donation reward.');
+      }
+    };
+    getBalance();
+  });
+
+  return { balance, vouchers, error };
+}
+
+export default useGetBalance;
