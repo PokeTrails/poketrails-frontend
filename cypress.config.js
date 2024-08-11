d67786c2eadc018a4e5254d2d046e0351f670732
RAbeyrathna <ra.github@rabeyrathna.com>
2024-08-11 19:33:54 +1000

    feat: Complete testing for login-signup workflow and export results into html

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,30 @@
 import { defineConfig } from "cypress";
+import mochawesome from 'mochawesome';
 
 export default defineConfig({
+  projectId: "hyjppy",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Set MochaAwesome as the reporter
+      on('before:run', () => {
+        config.reporter = 'mochawesome';
+        config.reporterOptions = {
+          charts: true,
+          overwrite: false,
+          html: false,
+          json: true,
+          reportDir: 'cypress/report/mochawesome-report',
+        };
+      });
+
+      // Implement other node event listeners here if needed
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
     },
   },
 });
